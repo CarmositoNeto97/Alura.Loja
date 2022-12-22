e703e6ed5c93086591a8b90e34b84e5d13a5ed4e
CarmositoNeto97 <carmozikaneto@outlook.com>
2022-12-22 16:46:57 -0300

    first commit

---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new LojaContext())
+            {
+                var produtos = context.Produtos.ToList();
+            }
+        }
+    }
+}
